7818cf84fff5c4b0409fac163e2aac184f60e37d
Justin Penny <justincpenny@hotmail.com>
2025-07-10 22:12:17 -0400

    added asset images and nav bar functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,56 @@
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
+import logo from '../../assets/logo.svg'
+import './navbar.css'
+import { useState } from 'react'
+
+
+// The use of <> to return multiple elements is called a React Fragment. Need to study more about React DOM
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#home">What is GPT? (Summerville?)</a></p>
+  <p><a href="#home">Open AI (Down town?)</a></p>
+  <p><a href="#home">Case Studies (Things to do?)</a></p>
+  <p><a href="#home">Library (History?)</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className="content_navbar">
-      <div className='content_navbar-links'>
-      Navbar
+    <div className="content__navbar">
+      <div className='content__navbar-links'>
+        <div className='content__navbar-links_logo'>
+          <img src={logo} alt="logo"/>
+          <div className='content__navbar-links_container'>
+            <Menu />
+          </div>
+        </div>
+      </div>
+      <div className='content__navbar-sign'>
+        <p>Sign In</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className='content__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+        }
+
+        {/* toggleMenu && -> if menu is toggled and the variable is true, execute block */}
+        {toggleMenu && (
+          <div className="content__navbar-menu_container scale-up-center">
+            <div className="content__navbar-menu_container-links">
+              <Menu />
+              <div className='content__navbar-menu_container-links-sign'>
+                <p>Sign In</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )
+        }
       </div>
     </div>
   )
